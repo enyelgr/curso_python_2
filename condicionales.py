ccdfca101ec7c499418351a99b756256b6e9197c
Enyelber <enyelbergerdes12@gmail.com>
2026-02-05 22:39:27 -0400

    [ADD] mi actualizacion de archivos

---
+++ condicionales.py
@@ -0,0 +1,15 @@
+nombre = input("Cual es tu nombre?")
+edad = input("Cual es tu edad?")
+pais = input("De que pais eres?")
+ciudad = input("De que ciudad eres?")
+parroquia = input("De que parroquia eres?")
+if nombre == ("enyelber") and edad == ("19") and pais == ("venezuela") and ciudad == ("caracas") and parroquia == ("el silencio"):
+    print("Eres del centro de caracas, y eres venezolano")
+elif nombre == ("carlos") and edad == ("27") and pais == ("colombia") and ciudad == ("cali") and parroquia == ("san fernando rey"):
+    print("Eres de cali, y eres colombiano")
+elif nombre == ("ana") and edad == ("22") and pais == ("argentina") and ciudad == ("buenos aires") and parroquia == ("palermo"):
+    print("Eres de buenos aires, y eres argentina")
+else:
+    print("No eres de los paises registrados")
+    print("No coincides con los perfiles registrados. !Gracias por participar¡")    
+    
---
+++ hello (Copiar).py
@@ -0,0 +1,74 @@
+Nombre = "Enyelber"
+Edad = "19"
+print("Hola, me llamo", Nombre, "y tengo", Edad, "años")
+
+num1 = 10
+num2 = 5
+print("Suma:", num1 + num2)
+print("Resta:", num1 - num2)
+print("Multiplicacion:", num1 * num2)
+print("Division:", num1 / num2)
+
+
+comida = "arroz chino"
+bebida = "cocacola"
+cubierto = "cucharilla"
+
+print("Me gusta comer", comida, "con", cubierto, "con refresco", bebida)
+
+altura_cm = 170
+altura_m = 1.7
+
+print("Mi altura en Cm es", altura_cm, "y mi altura en Mtrs es", altura_m)
+
+pan = 2
+leche = 3
+huevos = 5
+
+print("pan: $", pan)
+print("leche: $", leche)
+print("huevos: $", huevos)
+print("Total: $", pan + leche + huevos)
+
+mi_edad = 19
+diferencia_años = 2030 - 2026
+
+print("En 2030 tendre", mi_edad + diferencia_años, "años.")
+
+nota1 = 8
+nota2 = 9
+nota3 = 7
+
+print("Nota1:", nota1)
+print("Nota2:", nota2)
+print("Nota3:", nota3)
+print("Promedio:", (nota1 + nota2 + nota3) / 3)
+
+palabra = "Python"
+veces = 3
+
+print("Python" * 3)
+
+print("----Mi perfil----")
+
+nombre = "Enyelber"
+edad = 19
+ciudad = "Caracas"
+comida = "Pabellon"
+hermano = True
+casa = False
+trabajo = True
+futuro = "Si"
+hijo = False
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Ciudad:", ciudad)
+print("Comida Favorita:", comida)
+print("¿Tienes Hermanos?:", hermano)
+print("¿Tienes casa Propia?:", casa)
+print("¿Tienes Trabajo?:", trabajo)
+print("¿Tienes un futuro en Mente?:", futuro)
+print("¿Tienes Hijos?:", hijo)
+
+
